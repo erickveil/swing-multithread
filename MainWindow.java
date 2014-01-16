696f74af077293eba2111630067851e919e5be2e
eveil <eveil@comtechcom.net>
2014-01-16 09:17:45 -0800

    first commit
    gui layout

---
+++ MainWindow.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ * Created by eveil on 1/16/14.
+ */
+public class MainWindow {
+    private JButton startLoopButton;
+    private JTextField tb_ongoing;
+    private JButton getValueButton;
+    private JTextField tb_demand;
+    private JButton stopLoopButton;
+    private JPanel top_win;
+}
