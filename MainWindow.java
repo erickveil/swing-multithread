a74be2ff20172d5726521e61b322a4d9aa537a8a
eveil <eveil@comtechcom.net>
2014-01-16 10:13:13 -0800

    single threadded demonstrates the problem

--- MainWindow.java
+++ MainWindow.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by eveil on 1/16/14.
@@ -10,4 +12,64 @@ public class MainWindow {
     private JTextField tb_demand;
     private JButton stopLoopButton;
     private JPanel top_win;
+
+    public WorkerObject looper;
+
+    public MainWindow() {
+        looper= new WorkerObject();
+
+        startLoopButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try{
+                    runLoop();
+                }
+                catch(InterruptedException e){
+                    System.err.println(e.getMessage());
+                }
+                finally {
+                    System.out.println("Finished.");
+                }
+            }
+        });
+        getValueButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String strval=((Integer)looper.counter).toString();
+                tb_demand.setText(strval);
+            }
+        });
+        stopLoopButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+               looper.is_running=false;
+            }
+        });
+    }
+
+    public void runLoop() throws InterruptedException
+    {
+        looper.is_running=true;
+
+        while(looper.is_running){
+            looper.updateMe();
+            String strval=((Integer)looper.counter).toString();
+            System.out.println(strval);
+
+            // while this runs in the main thread, the gui will never even get a chance to redraw the text box with
+            // this set value.
+            tb_ongoing.setText(strval);
+
+            Thread.sleep(1000);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MainWindow");
+        frame.setContentPane(new MainWindow().top_win);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
---
+++ WorkerObject.java
@@ -0,0 +1,19 @@
+/**
+ * Created by eveil on 1/16/14.
+ */
+public class WorkerObject {
+    public boolean is_running;
+    public int counter;
+
+    public WorkerObject()
+    {
+        is_running=false;
+        counter=0;
+    }
+
+    public void updateMe()
+    {
+        //System.out.println("Counter value: "+counter);
+        ++counter;
+    }
+}
